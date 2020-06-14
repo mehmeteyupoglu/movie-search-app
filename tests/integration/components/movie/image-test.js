a4469c206a06c0893dfcca769ab4c856d70696d5
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-14 14:18:22 +0300

    Update testing images

--- tests/integration/components/movie/image-test.js
+++ tests/integration/components/movie/image-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { render, click } from '@ember/test-helpers';
 
 module('Integration | Component | movie/image', function(hooks) {
   setupRenderingTest(hooks);
@@ -17,5 +18,29 @@ module('Integration | Component | movie/image', function(hooks) {
   assert.dom('.image').exists();
   assert.dom('.image img').hasAttribute('src', '/assets/images/tmbd.png');
   assert.dom('.image img').hasAttribute('alt', 'TMDB');
+  });
+
+  test('clicking on the component toggles its size', async function(assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/tmbd.png"
+        alt="TMDB"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large')
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
